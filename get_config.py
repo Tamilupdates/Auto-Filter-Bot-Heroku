6baf70ade695192e58f00daf502f99843c436a3f
Tamil Updates <116391685+Tamilupdates@users.noreply.github.com>
2023-03-09 12:56:32 +0530

    Update get_config.py

--- get_config.py
+++ get_config.py
@@ -11,7 +11,7 @@ try:
     try:
         res = rget(CONFIG_FILE_URL)
         if res.status_code == 200:
-            with open('config.env', 'wb+') as f:
+            with open('info.py', 'wb+') as f:
                 f.write(res.content)
         else:
             LOGGER.error(f"Failed to download config.env {res.status_code}")
@@ -20,38 +20,5 @@ try:
 except Exception as e:
     LOGGER.error(e)
     pass
-load_dotenv(
-    "config.env",
-    override=True,
-)
-# tired of redeploying :(
-UPSTREAM_REPO = os.environ.get('UPSTREAM_REPO')
-UPSTREAM_BRANCH = os.environ.get('UPSTREAM_BRANCH')
-try:
-    if len(UPSTREAM_REPO) == 0:
-       raise TypeError
-except:
-    UPSTREAM_REPO = None
-try:
-    if len(UPSTREAM_BRANCH) == 0:
-       raise TypeError
-except:
-    UPSTREAM_BRANCH = None
 
-if UPSTREAM_REPO is not None:
-    if os.path.exists('.git'):
-        subprocess.run(["rm", "-rf", ".git"])
 
-    update = subprocess.run([f"git init -q \
-                     && git config --global user.email yashoswal18@gmail.com \
-                     && git config --global user.name mergebot \
-                     && git add . \
-                     && git commit -sm update -q \
-                     && git remote add origin {UPSTREAM_REPO} \
-                     && git fetch origin -q \
-                     && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
-
-    if update.returncode == 0:
-        LOGGER.info('Successfully updated with latest commit from UPSTREAM_REPO')
-    else:
-        LOGGER.warning('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
