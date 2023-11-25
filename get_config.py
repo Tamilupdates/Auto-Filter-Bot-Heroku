ba8da856fce2363b47f053c417ae8a46a2cb340e
Nandhakumar K <116391685+Tamilupdates@users.noreply.github.com>
2023-11-25 13:59:19 +0530

    Update get_config.py

--- get_config.py
+++ get_config.py
@@ -17,21 +17,4 @@ try:
 except Exception as e:
     LOGGER.error(e)
     pass
-
-
-UTILIS_FILE_URL = os.environ.get('UTILIS_FILE_URL')
-try:
-    if len(UTILIS_FILE_URL) == 0:
-        raise TypeError
-    try:
-        res = rget(UTILIS_FILE_URL)
-        if res.status_code == 200:
-            with open('utils.py', 'wb+') as f:
-                f.write(res.content)
-        else:
-            LOGGER.error(f"Failed to download info.py {res.status_code}")
-    except Exception as e:
-        LOGGER.error(f"UTILIS_FILE_URL: {e}")
-except Exception as e:
-    LOGGER.error(e)
-    pass
+    
