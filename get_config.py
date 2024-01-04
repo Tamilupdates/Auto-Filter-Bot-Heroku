b3fd7e115e9860c1b76a28d285447b839c5665a6
Nandhakumar K <116391685+Tamilupdates@users.noreply.github.com>
2024-01-04 16:49:30 +0530

    Update get_config.py

--- get_config.py
+++ get_config.py
@@ -1,23 +1,23 @@
+import logging
 from requests import get as rget
 import os
 
+logging.basicConfig(level=logging.ERROR)
+LOGGER = logging.getLogger(__name__)
+
 CONFIG_FILE_URL = os.environ.get('CONFIG_FILE_URL')
 try:
-    if len(CONFIG_FILE_URL) == 0:
-        raise TypeError
-    try:
-        res = rget(CONFIG_FILE_URL)
-        if res.status_code == 200:
-            with open('info.py', 'wb+') as f:
-                f.write(res.content)
-        else:
-            LOGGER.error(f"Failed to download info.py {res.status_code}")
-    except Exception as e:
-        LOGGER.error(f"CONFIG_FILE_URL: {e}")
-except Exception as e:
-    LOGGER.error(e)
-    pass
+    if not CONFIG_FILE_URL:
+        raise ValueError("CONFIG_FILE_URL is missing or empty")
 
+    res = rget(CONFIG_FILE_URL)
+    if res.status_code == 200:
+        with open('info.py', 'wb+') as f:
+            f.write(res.content)
+    else:
+        LOGGER.error(f"Failed to download info.py {res.status_code}")
+except Exception as e:
+    LOGGER.error(f"Error downloading CONFIG_FILE_URL: {e}")
 
 UTILS_FILE_URL = os.environ.get('UTILS_FILE_URL')
 if UTILS_FILE_URL:  # Check if UTILS_FILE_URL is provided
@@ -32,4 +32,3 @@ if UTILS_FILE_URL:  # Check if UTILS_FILE_URL is provided
         LOGGER.error(f"Error downloading UTILS_FILE_URL: {e}")
 else:
     LOGGER.info("UTILS_FILE_URL not provided. Skipping download.")
-    
